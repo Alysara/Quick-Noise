4d25d48458a34f60d9eda19030d4780e6aa95ee9
Alysara <alysara@proton.me>
2026-04-06 01:21:52 -0400

    Changed epsilon constant for perlin noise.

--- src/noise/perlin/constants.rs
+++ src/noise/perlin/constants.rs
@@ -6,8 +6,11 @@ pub const ROW_SIZE: usize = 32;
 pub const MAP_SIZE: usize = 1024;
 pub const VOL_SIZE: usize = 32768;
 
-pub const LO_EPSILON: f64 = 1e-4;
-pub const HI_EPSILON: f64 = 1.0 - 1e-4;
+// pub const LO_EPSILON: f64 = 1e-4;
+// pub const HI_EPSILON: f64 = 1.0 - 1e-4;
+
+pub const LO_EPSILON: f64 = 1e-5;
+pub const HI_EPSILON: f64 = 1.0 - 1e-5;
 
 pub type PerlinVec = SimdArray<f32, ROW_SIZE>;
 pub type PerlinMap = SimdArray<f32, MAP_SIZE>;
