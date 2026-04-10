cd6cdc0dc62ed47ace524168d4a8bc315dfec293
Alysara <alysara@proton.me>
2026-04-09 21:40:40 -0400

    Added module level x86 cfg.

--- src/simd/architectures/mod.rs
+++ src/simd/architectures/mod.rs
@@ -1,6 +1,9 @@
 pub mod intrinsics {
+    #[cfg(target_arch = "x86_64")]
     pub mod avx2;
+    #[cfg(target_arch = "x86_64")]
     pub mod sse;
+    #[cfg(target_arch = "x86_64")]
     pub mod avx512;
 
     #[cfg(target_arch = "aarch64")]
