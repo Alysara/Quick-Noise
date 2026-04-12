c4917191f9c16540cef53affab868e08f7e39c11
Alysara <alysara@proton.me>
2026-04-12 18:06:08 -0400

    Changed to scalar.

--- src/simd/arch_simd.rs
+++ src/simd/arch_simd.rs
@@ -22,9 +22,9 @@ cfg_if::cfg_if! {
     } else if #[cfg(all(target_arch = "x86_64", target_feature = "avx2"))] {
         pub const SIMD_WIDTH: usize = 32;
         pub const NUM_SIMD_REG: usize = 16;
-        pub type ArchSimd<T> = SimdVec<T, Avx2Family>;
-        pub type ArchMask<T> = SimdMask<T, Avx2Family>;
-        pub type ArchFamily = Avx2Family;
+        pub type ArchSimd<T> = SimdVec<T, ScalarFamily128>;
+        pub type ArchMask<T> = SimdMask<T, ScalarFamily128>;
+        pub type ArchFamily = ScalarFamily128;
     } else if #[cfg(all(target_arch = "x86_64", target_feature = "sse2"))] {
         pub const SIMD_WIDTH: usize = 16;
         pub const NUM_SIMD_REG: usize = 16;
