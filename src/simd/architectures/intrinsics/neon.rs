c7f63c1dc5acaa5f052a16c5c58e68f36982c040
Alysara <alysara@proton.me>
2026-04-14 13:52:11 -0400

    Fixed shifts for Neon.

--- src/simd/architectures/intrinsics/neon.rs
+++ src/simd/architectures/intrinsics/neon.rs
@@ -231,10 +231,31 @@ impl SimdNegateImpl for Neon {
 }
 
 impl SimdBlockByteShiftImpl for Neon {
-    #[inline(always)] fn block_left_byte_shift<const N: i32>(self) -> Self {
-        self_from_op!(vextq_u8, vdupq_n_u8(0), self, 16 - N)
+    // Stabilize const generic expr plssssss.
+    #[inline(always)]
+    fn block_left_byte_shift<const N: i32>(self) -> Self {
+        match N {
+            0 => self,
+            1 => self_from_const_op!(15, vextq_u8, Self::zero(), self),
+            2 => self_from_const_op!(14, vextq_u8, Self::zero(), self),
+            3 => self_from_const_op!(13, vextq_u8, Self::zero(), self),
+            4 => self_from_const_op!(12, vextq_u8, Self::zero(), self),
+            5 => self_from_const_op!(11, vextq_u8, Self::zero(), self),
+            6 => self_from_const_op!(10, vextq_u8, Self::zero(), self),
+            7 => self_from_const_op!(9, vextq_u8, Self::zero(), self),
+            8 => self_from_const_op!(8, vextq_u8, Self::zero(), self),
+            9 => self_from_const_op!(7, vextq_u8, Self::zero(), self),
+            10 => self_from_const_op!(6, vextq_u8, Self::zero(), self),
+            11 => self_from_const_op!(5, vextq_u8, Self::zero(), self),
+            12 => self_from_const_op!(4, vextq_u8, Self::zero(), self),
+            13 => self_from_const_op!(3, vextq_u8, Self::zero(), self),
+            14 => self_from_const_op!(2, vextq_u8, Self::zero(), self),
+            15 => self_from_const_op!(1, vextq_u8, Self::zero(), self),
+            16 => Self::zero(),
+            _ => unreachable!(),
+        }
     }
     #[inline(always)] fn block_right_byte_shift<const N: i32>(self) -> Self {
-        self_from_op!(vextq_u8, self, vdupq_n_u8(0), N)
+        self_from_const_op!(N, vextq_u8, self, Self::zero())
     }
 }
