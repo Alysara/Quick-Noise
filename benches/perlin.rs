2cbb539d161c480bc2b6df02cde2c71ff4becbeb
Alysara <alysara@proton.me>
2026-05-15 19:51:05 -0400

    Changed benches.

--- benches/perlin.rs
+++ benches/perlin.rs
@@ -484,8 +484,9 @@ fn simplex_3d_benchmark_fn2(c: &mut Criterion) {
     });
 }
 
+criterion_group!(benches, perlin_2d_benchmark, perlin_3d_benchmark);
 // criterion_group!(benches, perlin_2d_benchmark, perlin_3d_benchmark, perlin_2d_benchmark_fn2, perlin_3d_benchmark_fn2);
-criterion_group!(benches, perlin_2d_benchmark_batch, perlin_2d_benchmark_fn2, perlin_3d_benchmark_batch, perlin_3d_benchmark_fn2);
+// criterion_group!(benches, perlin_2d_benchmark_batch, perlin_2d_benchmark_fn2, perlin_3d_benchmark_batch, perlin_3d_benchmark_fn2);
 // criterion_group!(benches, simplex_2d_benchmark_batch, simplex_2d_benchmark_fn2);
 // criterion_group!(benches, value_2d_benchmark_batch, perlin_2d_benchmark_batch, simplex_2d_benchmark_batch);
 // criterion_group!(benches, cellular_2d_benchmark_batch, cellular_2d_benchmark_fn2);
