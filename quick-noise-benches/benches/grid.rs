577b1aad99b9ae45d9a53a230be6759817f762bf
Alysara <alysara@proton.me>
2026-07-24 21:53:13 -0400

    Separated benches into a separate crate so it neon bug with fastnoise2 doesn't happen.

--- quick-noise-benches/benches/grid.rs
+++ quick-noise-benches/benches/grid.rs
@@ -2,18 +2,16 @@ use std::hint::black_box;
 
 use criterion::{Criterion, Throughput, criterion_group, criterion_main};
 use quick_noise::{Fbm, Grid, Perlin, Value};
-use simply_simd::dispatch_simd;
 
 const SCALES: [f32; 11] = [64.0, 48.0, 32.0, 24.0, 16.0, 12.0, 8.0, 6.0, 4.0, 3.0, 2.0];
 
-#[dispatch_simd(A)]
 fn grid_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("grid_perlin_2d");
     group.throughput(Throughput::Elements(4096));
 
     let mut result = [0.0; 4096];
     for scale in SCALES {
-        let grid = Grid::<2, A>::new(64, 64);
+        let grid = Grid::<2>::new(64, 64);
 
         group.bench_function(format!("scale: {scale}"), |b| {
             b.iter(|| {
@@ -32,7 +30,7 @@ fn grid_benchmark(c: &mut Criterion) {
 
     let mut result = [0.0; 32768];
     for scale in SCALES {
-        let grid = Grid::<3, A>::new(32, 32, 32);
+        let grid = Grid::<3>::new(32, 32, 32);
 
         group.bench_function(format!("scale: {scale}"), |b| {
             b.iter(|| {
@@ -51,7 +49,7 @@ fn grid_benchmark(c: &mut Criterion) {
 
     let mut result = [0.0; 4096];
     for scale in SCALES {
-        let grid = Grid::<2, A>::new(64, 64);
+        let grid = Grid::<2>::new(64, 64);
 
         group.bench_function(format!("scale: {scale}"), |b| {
             b.iter(|| {
@@ -70,7 +68,7 @@ fn grid_benchmark(c: &mut Criterion) {
 
     let mut result = [0.0; 32768];
     for scale in SCALES {
-        let grid = Grid::<3, A>::new(32, 32, 32);
+        let grid = Grid::<3>::new(32, 32, 32);
 
         group.bench_function(format!("scale: {scale}"), |b| {
             b.iter(|| {
